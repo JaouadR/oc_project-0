ceed533de0b5963101dacf961e5e1d104f15b57d
JaouadR <jaouad.rady.enedis@gmail.com>
2018-08-13 15:16:33 +0200

    Adding 2 products to the list --> Ok

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,6 +22,8 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+
+		//adding 2 products to the list
 		products.put(++id, new Product(id, 80, 998.00, "Apple iPhone X", "Cell Phone "));
 		products.put(++id, new Product(id, 50, 98.95, "Jabra Evolve 40 USB UC MS Duo", "Stereo Headphones"));
 	}
