00b26a8df003c2bdfec0e263605d92192a816846
JaouadR <jaouad.rady.enedis@gmail.com>
2018-08-25 16:57:11 +0200

    New set for "public void setPrice", if a price is negative the value will set to 0.

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,6 +22,7 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, -32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+		products.put(++id, new Product(id, 15, 50.00, "TEST Product", "Test"));
 
 		//Adding 2 products to the list
 		products.put(++id, new Product(id, -40, 1158.00, "Apple iPhone X", "Cell Phone "));
