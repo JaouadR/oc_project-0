dca2b1976bb7720b0ce5ea0ef53b16ba47343ea4
JaouadR <jaouad.rady.enedis@gmail.com>
2018-08-25 14:24:08 +0200

    Second pass test of a negative price value to check the "zero" display --> OK

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -20,7 +20,7 @@ public class ProductRepository {
 		products.put(++id, new Product(id, -10, 92.50, "Echo Dot", "(2nd Generation) - Black"));
 		products.put(++id, new Product(id, 20, -9.99, "Anker 3ft / 0.9m Nylon Braided", "Tangle-Free Micro USB Cable"));
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
-		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
+		products.put(++id, new Product(id, 40, -32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
 
 		//Adding 2 products to the list
